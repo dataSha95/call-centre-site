521004547ad7d1b377b07d18193752dc87eb842b
dataSha95 <shahedpremmium@gmail.com>
2025-05-07 01:10:39 +0000

    Step 3: Added About and Contact pages with form saving to database

--- backend/urls.py
+++ backend/urls.py
@@ -5,4 +5,8 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ContactForm
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'thankyou.html')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
+    
+def about(request):
+    return render(request, 'about.html')
